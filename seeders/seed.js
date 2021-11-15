201dbf126e4dc980fff669bbbe5677278a1307d5
Evelyn G <evelyn.gmaldonado@gmail.com>
2021-11-14 16:33:10 -0800

    creating a more orginzed structure and exploring

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,12 @@
+const express = require("express");
 const mongoose = require('mongoose');
 const db = require('../models');
 
-mongoose.connect('mongodb://localhost/workout', {
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
